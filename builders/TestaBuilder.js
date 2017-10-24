229b3e6f64cb4500c160c151baf199818e4dc1ec
pburagina <pburagina@pburagina-ThinkPad-E560>
2017-10-24 08:24:24 +0200

    record testa IM

--- builders/TestaBuilder.js
+++ builders/TestaBuilder.js
@@ -3,15 +3,12 @@ var Record = require('../record_model/Record.js');
 
 function TestaBuilder(sia, abi){
 	
-	this.fieldEmpty =  new Field(1, " ");
-	this.fieldTipoRecord = new Field(2, "IM");
-	this.fieldSia = new Field(5, sia);
-	this.fieldAbi = new Field(5, abi);
-	this.testa = new Record();
-	this.testa.addField(this.fieldEmpty);
-	this.testa.addField(this.fieldTipoRecord);
-	this.testa.addField(this.fieldSia);
-	this.testa.addField(this.fieldAbi);
+	this.fieldEmpty =  new Field(1, "", "right", " ");
+	this.fieldTipoRecord = new Field(2, "IM", "right", " ");
+	this.fieldSia = new Field(5, sia, "right", " ");
+	this.fieldAbi = new Field(5, abi, "right", " ");
+	
+
 	
 	this.date = new Date();
 	this.dateString = "";
@@ -22,8 +19,29 @@ function TestaBuilder(sia, abi){
 		this.dateString += "0".concat(new Number(this.date.getMonth()+ 1).toString());
 	this.dateString += this.date.getFullYear().toString().substr(2,4);
 	this.dateField = new Field(6,this.dateString);
-	this.testa.addField(this.dateField);
+	this.nomeSupportoField = new Field(20,"", "right", " ");
+	this.campoADisposizioneField = new Field(6, "", "right", " ");
+	this.fieldEmpty59 =  new Field(59, "", "right", " ");
+	this.fieldEmpty7 =  new Field(7, "", "right", " ");
+	this.fieldEmpty2 =  new Field(2, "", "right", " ");
+	this.fieldDivisa =  new Field(1, "E", "right", " ");
+	this.fieldEmpty1 =  new Field(1, "", "right", " ");
+	this.fieldNonDisp =  new Field(5, "", "right", " ");
 	
+	this.testa = new Record();
+	this.testa.addField(this.fieldEmpty);
+	this.testa.addField(this.fieldTipoRecord);
+	this.testa.addField(this.fieldSia);
+	this.testa.addField(this.fieldAbi);
+	this.testa.addField(this.dateField);
+	this.testa.addField(this.nomeSupportoField);
+	this.testa.addField(this.campoADisposizioneField);
+	this.testa.addField(this.fieldEmpty59);
+	this.testa.addField(this.fieldEmpty7);
+	this.testa.addField(this.fieldEmpty2);
+	this.testa.addField(this.fieldDivisa);
+	this.testa.addField(this.fieldEmpty1);
+	this.testa.addField(this.fieldNonDisp);
 	
 	console.log(this.testa.toCBI());
 }
--- record_model/Field.js
+++ record_model/Field.js
@@ -2,9 +2,22 @@
  * Object for fields of every record in a CBI file, using Module design pattern
  */
 
-function Field(length, val){
+function Field(length, val, pad, charPad){
 	this.lenght = length;
 	this.val = val;
+	if(val.length < length){
+		if(pad == "right"){
+			for(var i = 0; i < length - val.length; i++){
+				this.val = this.val + charPad;
+			}
+		}else if(pad == "left"){
+			for(var i = 0; i < length - val.length; i++){
+				this.val = charPad + this.val;
+			}
+		}
+	}else if(val.length > length){
+		this.val = this.val.substr(0,length);
+	}
 }
 
 Field.prototype.toString = function(){
